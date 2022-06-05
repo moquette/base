0bb7829f0f8e88da703a0c9b64430dcfb9e78303
Joaquin A. Moquette <joaquin@moquette.us>
2022-06-05 09:01:20 -0700

    fix: env

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,6 @@
 module.exports = (api) => {
   //const mode = process.env.NODE_ENV ?? 'production'
-  export const mode = process.env.NODE_ENV ? process.env.NODE_ENV : 'production'
+  const mode = process.env.NODE_ENV ? process.env.NODE_ENV : 'production'
 
   // This caches the Babel config by environment.
   api.cache.using(() => mode)
